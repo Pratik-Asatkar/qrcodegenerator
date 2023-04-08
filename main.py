dba569bb8dc6758e62f67bbe7dfb85fbad321032
Pratik Asatkar <praikasatkar11@gmail.com>
2023-04-09 05:18:39 +0530

    new way of generating qr using flask

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-import segno 
-qrcode = segno.make_qr("www.theverge.com")
-qrcode.save('theverge.png',scale=10)
+from flask import Flask, request, send_file
+from flask_cors import CORS
+
+import qrcode
+
+from io import BytesIO
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/generate_qrcode', methods=['POST'])
+def generate_qrcode():
+    buffer = BytesIO()
+    data = request.form.get('data')
+
+    img = qrcode.make(data)
+    img.save(buffer)
+    buffer.seek(0)
+
+    response = send_file(buffer, mimetype='image/png')
+    return response
+
+if __name__ == '__main__':
+    app.run()
